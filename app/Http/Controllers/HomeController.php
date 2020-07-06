27ed707ba2e5559aee14872dc24666b8df311f0a
Abdulaziz-al <abdoal0aziz133@gmail.com>
2020-07-06 21:24:44 +0300

    first_comment

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        toast('Your Post as been submited!','success');
+        return view('home');
+    }
+
+    protected function welcome(){
+        toast('Question Toast','question');
+        return view('welcome');
+    }
+}
