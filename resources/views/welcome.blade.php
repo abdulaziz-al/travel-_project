1941b79783bc5e799777d8223645c103af2f3143
Abdulaziz-al <abdoal0aziz133@gmail.com>
2020-07-08 22:49:30 +0300

    save

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,69 +1,24 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+@extends('layouts.app')
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+@section('content')
 
-            .position-ref {
-                position: relative;
-            }
+<div class="container">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
+<div class="select-box">
+    
+    <label for="select-box1" class="label select-box1"><span class="label-desc">Choose your country</span> </label>
+    <select id="select-box1" class="select">
+      <option value="Choice 1">Falkland Islands</option>
+      <option value="Choice 2">Germany</option>
+      <option value="Choice 3">Neverland</option>
+    </select>
+    
+  </div>
         @include('sweetalert::alert')
 
         <div class="flex-center position-ref full-height">
@@ -98,5 +53,5 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+    @endsection
