fc0685a893b51564c972ae6dfca42f20b708dbc4
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2020-07-20 18:07:46 +0300

    k

--- public/js/app.js
+++ public/js/app.js
@@ -60071,8 +60071,8 @@ var routes = [{
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ezooa\Desktop\other company\travel_project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ezooa\Desktop\other company\travel_project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\fss6s\Desktop\terhal\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\fss6s\Desktop\terhal\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
